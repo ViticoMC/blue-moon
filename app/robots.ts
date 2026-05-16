de72fdb93ba5bf0d23457cf069ee28687ac18fd4
ViticoMC <victormanuelmartinezcampo178@gmail.com>
2026-05-16 02:18:21 -0400

    ajustes en el archivo robots y eliminación del sitemap, se simplifican las reglas de indexación

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin/login", "/admin/dashboard"],
+      disallow: ["/admin"],
     },
     sitemap: "https://blue-moon-snowy.vercel.app/sitemap.xml",
   };
